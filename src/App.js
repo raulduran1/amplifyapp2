a3539252fb749408d6bc30504c3def295e5eab22
raulduran1 <raulduran@iradt.com>
2020-11-09 17:42:06 -0500

    Changes for v2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -17,6 +18,23 @@ function App() {
         >
           Learn React
         </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
